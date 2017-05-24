b097b12a57751f8d84f72799cbdb019b9ff9003e
Justes <2395781272@qq.com>
2017-05-24 08:24:35 +0800

    mod bayes

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
+from numpy import *
 
 def loadDataSet():
     postingList = [['my','dog','has','flea','problems','help','please'],
             ['maybe','not','take','him','to','dog','park','stupid'],
-            ['my','dalmation','is','so','cute','I','low','him'],
+            ['my','dalmation','is','so','cute','I','love','him'],
             ['stop','posting','stupid','worthless','garbage'],
             ['my','licks','ate','my','steak','how','to','stop','him'],
             ['quit','buying','worthless','dog','food','stupid']
@@ -25,3 +26,22 @@ def setOfWords2Vec(vocabList,inputSet):
         else:
             print "the word: %s is not in my Vocabulary!" % word
     return returnVec
+
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = zeros(numWords) # 初始化概率
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i] # 向量相加
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i]) 
+    p1Vect = p1Num / p1Denom # 对每个元素做除法
+    p0Vect = p0Num / p0Denom
+    return p0Vect,p1Vect,pAbusive
