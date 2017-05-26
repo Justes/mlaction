e035dce56e9f8641ab58fb33ed1f3bab9fbb3c24
Justes <2395781272@qq.com>
2017-05-26 08:26:26 +0800

    add spam test

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -18,11 +18,11 @@ def createVocabList(dataSet):
         vocabSet = vocabSet | set(document) # 创建两个集合的并集
     return list(vocabSet)
 
-def setOfWords2Vec(vocabList,inputSet):
+def bagOfWords2Vec(vocabList,inputSet):
     returnVec = [0] * len(vocabList) # 创建一个其中所含元素都为0的向量
     for word in inputSet:
         if word in vocabList:
-            returnVec[vocabList.index(word)] = 1
+            returnVec[vocabList.index(word)] += 1
         else:
             print "the word: %s is not in my Vocabulary!" % word
     return returnVec
@@ -67,3 +67,42 @@ def testingNB():
     testEntry = ['stupid','garbage']
     thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
     print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
+
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W*',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet = []
+    for i in range(10):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList,docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+            print "classification error",docList[docIndex]
+    print 'the error rate is : ',float(errorCount)/len(testSet)
